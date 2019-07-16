b440f72f2e0cb33f58c7472b6f83bd3fcb2a2e7e
BaeJi77 <hahawjstk@gmail.com>
2019-07-16 20:04:46 +0900

    Update Trainer data logic.

--- services/trainerService.js
+++ services/trainerService.js
@@ -10,16 +10,19 @@ module.exports = {
         return await trainerRepository.findTrainer(trainerId);
     },
 
-    registerNewTrainer: async function(newTrainerInformation) {
+    registerNewTrainer: async function(newTrainerInformation, profilePicture, activityPictures) {
         //TODO: S3 사진 업로드 모듈 만들기
+        //TODO: trainer 등록 후에 trainerPicture 추가해서 연결해주기
+
         return await trainerRepository.createTrainer(newTrainerInformation);
     },
 
     updateTrainerBySelf: async function(trainerId, updateTrainerInformationInRequest) {
+        //TODO: find 후에 사진 있는 경우 업데이트, 없는 경우 Create
         return await trainerRepository.updateTrainer(trainerId, updateTrainerInformationInRequest);
     },
 
-    updateFCMTokenOfTrainer: async function(trainerId, FCMTokenValue) {
-        return await trainerRepository.updateFCMToken(trainerId, FCMTokenValue);
+    updateFcmTokenOfTrainer: async function(trainerId, FcmToken) {
+        return await trainerRepository.updateFcmToken(trainerId, FcmToken);
     }
 };
