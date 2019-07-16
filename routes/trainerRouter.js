8686f16bced8e6575a4a27f19de33b73f05abcde
BaeJi77 <hahawjstk@gmail.com>
2019-07-16 15:01:53 +0900

    Complete trainer logic.

--- routes/trainerRouter.js
+++ routes/trainerRouter.js
@@ -5,10 +5,34 @@ const trainerService = require('../services/trainerService');
 
 
 router.get('/', getAllTrainerList);
+router.post('/', makeNewTrainer);
+router.get('/:trainerId', getCertainTrainer);
+router.put('/:trainerId', updateTrainer);
+router.post('/:trainerId', setFCMToken);
 
 
 async function getAllTrainerList(req, res, next) {
     res.status(200).send(await trainerService.findAllTrainersOrderByRecognition());
+}
+
+
+async function getCertainTrainer(req, res, next) {
+    let trainerId = req.params.trainerId;
+    res.status(200).send(await trainerService.findCertainTrainer(trainerId));
+}
+
+async function makeNewTrainer(req, res, next) {
+    res.status(201).send(await trainerService.registerNewTrainer())
+}
+
+async function updateTrainer(req, res, next) {
+    let trainerId = req.params.trainerId;
+    res.status(204).send(await trainerService.updateTrainerBySelf(trainerId, req.body))
+}
+
+async function setFCMToken(req, res, next) {
+    let trainerId = req.params.trainerId;
+    res.status(201).send(await trainerService.updateFCMTokenOfTrainer(trainerId, req.body));
 }
 
 
