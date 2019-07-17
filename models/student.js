6660051335238588ef7058ef88d75a66a90813a8
BaeJi77 <hahawjstk@gmail.com>
2019-07-17 17:12:35 +0900

    Define student table.

---
+++ models/student.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('student', {
+        username: DataTypes.STRING,
+        email: DataTypes.STRING,
+        age: {
+            type: DataTypes.INTEGER,
+            min: 1
+        },
+        picture_url: DataTypes.STRING,
+        gender: {
+            type: DataTypes.ENUM(
+                '남성', '여성', '대답을 원하지 않음'
+            ),
+        },
+        phone_number: DataTypes.STRING,
+        fcm_token: DataTypes.STRING,
+        access_token: DataTypes.STRING
+    }, {
+        timestamps: true
+    })
+};
