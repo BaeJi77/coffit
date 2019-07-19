d9a66f196f4190db39d7827086397eff529abde3
BaeJi77 <hahawjstk@gmail.com>
2019-07-19 18:26:27 +0900

    Update image upload naming logic. Define banner table.

---
+++ models/banner.js
@@ -0,0 +1,8 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('banner', {
+        image_url: DataTypes.STRING,
+        thumbnail_url: DataTypes.STRING
+    }, {
+        timestamps: true
+    })
+}
