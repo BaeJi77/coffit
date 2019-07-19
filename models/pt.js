8462b3aa20c9c28831f9b21d065b98bf40952fb8
BaeJi77 <hahawjstk@gmail.com>
2019-07-19 12:46:02 +0900

    Complete pt model define.

--- models/pt.js
+++ models/pt.js
@@ -1,15 +1,15 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('pt', {
-        state: {
-            type: DataTypes.ENUM(
-                'training', 'finished', 'scheduling'
-            )
-        },
+        state: DataTypes.INTEGER,
         price: DataTypes.INTEGER,
         total_number: DataTypes.INTEGER,
         rest_number: DataTypes.INTEGER,
         start_date: DataTypes.DATE,
         end_date: DataTypes.DATE,
+        pt_room: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4
+        }
     }, {
         timestamps: true
     })
