4a79f76b1b68b0120ada9d257cac21e363aee94c
BaeJi77 <hahawjstk@gmail.com>
2019-07-19 14:13:10 +0900

    Make faker date for pts. Update swagger ui.

--- config/faker.js
+++ config/faker.js
@@ -2,6 +2,7 @@ const faker = require('faker');
 const trainerRepository = require('../repositories/trainerRepository');
 const trainerPictureRepository = require('../repositories/trainerPictureRepository');
 const studentRepository = require('../repositories/studentRepository');
+const ptRepository = require('../repositories/ptRepository');
 
 module.exports = {
     makeFakeTrainerData: async function() {
@@ -34,6 +35,21 @@ module.exports = {
             obj.phone_number = faker.phone.phoneNumberFormat();
             obj.gender = i % 2 === 0 ? '남성' : '여성';
             await studentRepository.createStudent(obj);
+        }
+
+        // TODO: pt 데이터 넣기
+        for(var i = 1 ; i <= 5 ; i++) {
+            var obj = {};
+            obj.state = faker.random.number(1);
+            // obj.pt_room = faker.random.alphaNumeric(15);
+            obj.price = faker.finance.account();
+            obj.total_number = 8;
+            obj.rest_number = i;
+            obj.start_date = faker.date.recent();
+            obj.end_date = faker.date.future(1, '2019-07-20');
+            obj.studentId = i;
+            obj.trainerId = 6-i;
+            await ptRepository.createNewPt(obj);
         }
     }
 };
