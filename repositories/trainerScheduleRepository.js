3c2cfef97fa2d3730132dec3fa93736fdc64f366
BaeJi77 <hahawjstk@gmail.com>
2019-07-22 20:28:08 +0900

    fix repository letter and timezone utc to kor. And add faker data.

--- repositories/trainerScheduleRepository.js
+++ repositories/trainerScheduleRepository.js
@@ -1,13 +1,13 @@
-const {trainerSchedule} = require('../models');
+const {TrainerSchedule} = require('../models');
 
 module.exports = {
     createNewTrainerSchedule: async function(newTrainerScheduleInformation) {
-        return await trainerSchedule.create(newTrainerScheduleInformation);
+        return await TrainerSchedule.create(newTrainerScheduleInformation);
     },
 
     // TODO: schedule logic에서 반드시 어떻게 바꿀지 확인해서 state에 값 넣어주기.
     updateTrainerScheduleAvailable: async function(trainerScheduleId, availableState) {
-        return await trainerSchedule.update({
+        return await TrainerSchedule.update({
             available: availableState
         }, {
             where: {
