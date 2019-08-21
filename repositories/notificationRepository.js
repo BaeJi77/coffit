416b922ade3b2451ce2ecab756c776857feb3b9b
BaeJi77 <hahawjstk@gmail.com>
2019-08-21 20:48:12 +0900

    Add logger to pt comment logic Add logger when success to new pt comment in ptCommentService.js Add logger when calling to new pt comment in ptCommentRepository.js

--- repositories/notificationRepository.js
+++ repositories/notificationRepository.js
@@ -5,7 +5,7 @@ const logger = require('../config/logger');
 module.exports = {
     // TODO: FCM 연동하여서 알람 내용과 같이 push 알람 가도록
     createNewNotification: async function(newNotification) {
-        logger.info('[notificationRepository.js] [createNewNotification] make new notification');
+        logger.info('[notificationRepository.js] [createNewNotification] Call createNewNotification method');
         logger.info(newNotification);
         return await Notification.create(newNotification);
     },
