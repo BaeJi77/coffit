29cabf829f260a66b70366439456ef3e7d5633a3
BaeJi77 <hahawjstk@gmail.com>
2019-08-30 01:07:12 +0900

    Add log pt logic add log at router, service.

--- repositories/ptRepository.js
+++ repositories/ptRepository.js
@@ -4,6 +4,8 @@ const {Student} = require('../models');
 
 const {Op} = require('../models');
 
+const logger = require('../config/logger');
+
 module.exports = {
     findAllStudentIdUsingTrainerId: async function(trainerId) {
         return await Pt.findAll({
@@ -41,6 +43,8 @@ module.exports = {
     },
 
     createNewPt: async function(newPtInformation) {
+        logger.info('[ptRepository] [createNewPt] create new pt.');
+        logger.info(newPtInformation);
         return await Pt.create(newPtInformation);
     },
 
@@ -57,15 +61,8 @@ module.exports = {
         })
     },
 
-    findCertainPt: async function(ptId) {
-        return await Pt.findOne({
-            where: {
-                id: ptId
-            }
-        })
-    },
-
     decreaseRestPtNumberAfterPt: async function(ptId) {
+        logger.info('[ptRepository] [decreaseRestPtNumberAfterPt] When pt is finished, rest pt count is decreased. ptId: %d', ptId);
         return await Pt.decrement('rest_number', {
             where: {
                 id: ptId
@@ -73,17 +70,3 @@ module.exports = {
         });
     }
 };
-
-
-/*
-* update에 대한 다양한 상황 모두 체크
-* 1. state가 바뀌는 경우
-*   - 현재 운동중인 상태 -> training, 보통에는 waiting
-*   - rest_number가 다 끝남 -> finished
-*   - end_date가 지남 -> closed -> 12시 기준 -> job schedule을 통해서 확인해봅시다.
-*   - 서로 스케줄 조정 중 -> scheduling
-*   - Pt가 끝났을 때 -> rest_number 떨어트리고, state: waiting으로 바꾸기
-* 2. 스케줄이 바꿔서 그것에 해당하는 pt정보가 바뀌어야 할 경우
-*   - ????
-* */
-
--- routes/ptRouter.js
+++ routes/ptRouter.js
@@ -3,40 +3,39 @@ var router = express.Router();
 
 const ptService = require('../services/ptService');
 
+const logger = require('../config/logger');
 
 router.get('/students/:studentId', getAllPtsOfStudent);
 async function getAllPtsOfStudent(req, res, next) {
     let studentId = req.params.studentId;
-    await ptService.findOnePtsOfStudentUsingStudentId(studentId)
-        .then(result => {
-            res.status(200).send(result);
-        })
-        .catch(err => {
-            next(err);
-        });
+    try {
+        logger.info('[ptRouter] [getAllPtsOfStudent] find all pt list of student. studentId: %d', studentId);
+        res.status(200).send(await ptService.findOnePtsOfStudentUsingStudentId(studentId));
+    } catch (err) {
+        next(err);
+    }
 }
 
 router.get('/trainers/:trainerId', getAllPtsOfTrainer);
 async function getAllPtsOfTrainer(req, res, next) {
     let trainerId = req.params.trainerId;
-    await ptService.findAllPtsOfTrainerUsingTrainerId(trainerId)
-        .then(result => {
-            res.status(200).send(result);
-        })
-        .catch(err => {
-            next(err);
-        });
+    try {
+        logger.info('[ptRouter] [getAllPtsOfTrainer] find all pt list of trainer. trainerId: %d', trainerId);
+        res.status(200).send(await ptService.findAllPtsOfTrainerUsingTrainerId(trainerId));
+    } catch (err) {
+        next(err);
+    }
 }
 
 router.post('/', postNewPt);
 async function postNewPt(req, res, next) {
-    await ptService.makeNewPt(req.body)
-        .then(result => {
-            res.status(200).send(result);
-        })
-        .catch(err => {
-            next(err);
-        });
+    try {
+        logger.info('[ptRouter] [postNewPt] create new pt');
+        logger.info(req.body);
+        res.status(200).send(await ptService.makeNewPt(req.body));
+    } catch (err) {
+        next(err);
+    }
 }
 
 
