9ba44f0083179c087f57b519c09557911b0b950e
BaeJi77 <hahawjstk@gmail.com>
2019-08-30 17:39:07 +0900

    Add log student logic add log at studentRouter, studentRepository. and change method name setPictureUrl that decide picture url using object undefined.

--- repositories/studentRepository.js
+++ repositories/studentRepository.js
@@ -4,6 +4,7 @@ const {Op} = require('../models');
 
 module.exports = {
     findAllStudentUsingStudentIdArray: async function(studentIdArray) {
+        logger.info('[studentRepository] [findAllStudentUsingStudentIdArray] find certain student. studentIds: %s', studentIdArray.toString());
         return await Student.findAll({
             where: {
                 id: {
@@ -14,6 +15,8 @@ module.exports = {
     },
 
     findAllStudentOfTrainerSearchingStudentName: async function(studentIdArray, studentName) {
+        logger.info('[studentRepository] [findAllStudentOfTrainerSearchingStudentName] find certain student. studentIds: %s', studentIdArray.toString());
+        logger.info('searchName: %s', studentName);
         return await Student.findAll({
             where: {
                 id: {
@@ -27,6 +30,7 @@ module.exports = {
     },
 
     findStudentUsingStudentId: async function(studentId) {
+        logger.info('[studentRepository] [findStudentUsingStudentId] find certain student. studentId: %d', studentId);
         return await Student.findOne({
             where: {
                 id: studentId
@@ -35,10 +39,14 @@ module.exports = {
     },
 
     createStudent: async function(newStudent) {
+        logger.info('[studentRepository] [createStudent] register new student');
+        logger.info(newStudent);
         return await Student.create(newStudent);
     },
 
     updateStudent: async function(studentId, updateStudent){
+        logger.info('[studentRepository] [updateStudent] update student information. studentId: %d', studentId);
+        logger.info(updateStudent);
         return await Student.update(updateStudent, {
             where:{
                 id: studentId
@@ -47,6 +55,8 @@ module.exports = {
     },
 
     updateStudentFcmToken: async function(studentId, FcmToken) {
+        logger.info('[studentRepository] [updateStudentFcmToken] update fcm token. studentId: %d', studentId);
+        logger.info('Fcmtoken: %s', FcmToken);
         return await Student.update(FcmToken, {
             where: {
                 id: studentId
--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-const upload = require('../modules/image_upload');
-
 const studentService = require('../services/studentService');
+
+const upload = require('../modules/image_upload');
+const logger = require('../config/logger');
 
 
 router.post('/', upload.single('profilePicture'), makeNewStudent);
 async function makeNewStudent(req, res, next) {
     try {
+        logger.info('[studentRouter] [makeNewStudent] make new student');
+        logger.info(req.body);
+        logger.info(req.file);
         res.status(201).send(await studentService.makeNewStudent(req.body, req.file));
     } catch (err) {
         next(err);
@@ -19,6 +23,7 @@ router.get('/:studentId', getStudentProfile);
 async function getStudentProfile(req, res, next) {
     let studentId = req.params.studentId;
     try {
+        logger.info('[studentRouter] [getStudentProfile] find student detail. studentId: %d', studentId);
         res.status(200).send(await studentService.findCertainStudent(studentId));
     } catch (err) {
         next(err);
@@ -29,16 +34,21 @@ router.put('/:studentId', upload.single('profilePicture'), updateStudent);
 async function updateStudent(req, res, next) {
     let studentId = req.params.studentId;
     try {
+        logger.info('[studentRouter] [updateStudent] update student information. studentId: %d', studentId);
+        logger.info(req.body);
+        logger.info(req.file);
         res.status(204).send(await studentService.updateStudentUsingStudentId(studentId, req.body, req.file));
     } catch (err) {
         next(err);
     }
 }
 
-router.post('/:studentId/token', updateFcmToken);
-async function updateFcmToken(req, res, next) {
+router.post('/:studentId/token', setFcmToken);
+async function setFcmToken(req, res, next) {
     let studentId = req.params.studentId;
     try {
+        logger.info('[studentRouter] [setFcmToken] set fcm token. studentId : %d', studentId);
+        logger.info(req.body);
         res.status(201).send(await studentService.updateFcmTokenOfStudent(studentId, req.body));
     } catch (err) {
         next(err);
@@ -50,6 +60,7 @@ async function getAllStudentOfTrainerAndSearching(req, res, next) {
     let trainerId = req.params.trainerId;
     let searchStudentName = req.query.studentName;
     try {
+        logger.info('[studentRouter] [getAllStudentOfTrainerAndSearching] find student of trainer sometime search studentName. studentName : %s', searchStudentName);
         res.status(200).send(await studentService.findAllStudentOfTrainerOrSearchedStudents(trainerId, searchStudentName));
     } catch (err) {
         next(err);
