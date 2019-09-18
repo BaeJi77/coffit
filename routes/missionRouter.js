1c2c7e44b95a3126fb42d8be4d6c16258d275564
BaeJi77 <hahawjstk@gmail.com>
2019-09-18 22:01:50 +0900

    Implement missionRouter I can't test this logic yet. Checking dumy data. plz.

--- routes/missionRouter.js
+++ routes/missionRouter.js
@@ -2,11 +2,65 @@ var express = require('express');
 var router = express.Router();
 
 const generatePreSignedUrl = require('../modules/generate_preSignedUrl');
+const missionService = require('../services/missionService');
+
+const logger = require('../config/logger');
 
 router.get('/preUrl', getPreSignedUrl);
 async function getPreSignedUrl(req, res, next) {
-    let preSignedObject = await generatePreSignedUrl.getPostPreSignedUrl(req, res);
-    res.status(200).send(preSignedObject);
+    try {
+        logger.info('[missionRouter] [getPreSignedUrl] get aws s3 pre signed url');
+        let preSignedObject = await generatePreSignedUrl.getPostPreSignedUrl(req, res);
+        logger.info(preSignedObject);
+        res.status(200).send(preSignedObject);
+    } catch (e) {
+        next(e);
+    }
+}
+
+router.post('/', makeNewMission);
+async function makeNewMission(req, res, next) {
+    try {
+        logger.info('[missionRouter] [makeNewMission] make new mission');
+        logger.info(req.body);
+        res.status(201).send(await missionService.makeNewMission(req.body));
+    } catch (e) {
+        next(e);
+    }
+}
+
+router.get('/:missionId', getMissionDetail);
+async function getMissionDetail(req, res, next) {
+    try {
+        let missionId = req.params.missionId;
+        logger.info('[missionRouter] [getMissionDetail] get mission detail using mission id. missionId: %d', missionId);
+        res.status(200).send(await missionService.findMissionDetail(missionId));
+    } catch (e) {
+        next(e);
+    }
+}
+
+router.put('/:missionId', updateMission);
+async function updateMission(req, res, next) {
+    try {
+        let missionId = req.params.missionId;
+        logger.info('[missionRouter] [updateMission] update certain mission. missionId: %d', missionId);
+        logger.info(req.body);
+        res.status(204).send(await missionService.updateCommentAndRateInMission(missionId, req.body));
+    } catch (e) {
+        next(e);
+    }
+}
+
+router.get('/students/:studentId', getMissionOfStudent);
+async function getMissionOfStudent(req, res, next) {
+    try {
+        let studentId = req.params.studentId;
+        logger.info('[missionRouter] [getMissionOfStudent] find all mission of target student. studentId: %d', studentId);
+        res.status(200).send(await missionService.findAllMissionOfCertainStudent(studentId));
+    } catch (e) {
+        next(e);
+    }
 }
 
 module.exports = router;
