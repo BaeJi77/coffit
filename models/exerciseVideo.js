420f2e3817e50f2d79ebe9e15c34d98d1dc795b3
BaeJi77 <hahawjstk@gmail.com>
2019-09-22 16:53:32 +0900

    Add key_name to exerciseVideo table And update swagger.

--- models/exerciseVideo.js
+++ models/exerciseVideo.js
@@ -2,6 +2,7 @@ module.exports = (sequelize, DataTypes) => {
     return sequelize.define('exerciseVideo', {
         date: DataTypes.DATE,
         url: DataTypes.STRING,
+        key_name: DataTypes.STRING,
         thumbnail_url: DataTypes.STRING,
         time_tag: DataTypes.STRING,
         student_id: DataTypes.INTEGER,
