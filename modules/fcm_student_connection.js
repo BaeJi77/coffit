27d7ed965166e282bea1b873f0fe50c953bbc125
BaeJi77 <hahawjstk@gmail.com>
2019-10-17 17:08:57 +0900

    Combine fcm connect logic. Have to check fcm_key.json

--- modules/fcm_student_connection.js
+++
@@ -1,6 +0,0 @@
-const admin = require('firebase-admin');
-const fcm_key = require('../config/fcm_key');
-
-module.exports.studentPush = admin.initializeApp({
-    credential: admin.credential.cert(fcm_key.student_key)
-});
--- modules/fcm_trainer_connection.js
+++
@@ -1,6 +0,0 @@
-const admin = require('firebase-admin');
-const fcm_key = require('../config/fcm_key');
-
-module.exports.trainerPush = admin.initializeApp({
-    credential: admin.credential.cert(fcm_key.trainer_key)
-});
