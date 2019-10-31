84e7f9eebb3c72c8cf5374f0beec8807ca2df4da
BaeJi77 <hahawjstk@gmail.com>
2019-10-31 20:20:32 +0900

    Configure socket.io - make namespace (/chattings) - emit or on event   - connection   - joinRoom   - closeRoom   - sendMessage

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 
-router.get('/', getIndex);
+// router.get('/', getIndex);
+//
+//
+// function getIndex (req, res, next) {
+//     res.render('index', { title: 'Express' });
+// }
 
-
-function getIndex (req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/chatting/trainers', getChattingTrainer);
+function getChattingTrainer(req, res, next) {
+    fs.readFile(__dirname + '/../views/chatting_trainer.html', (err, data) => {
+        res.end(data, 'utf-8');
+    });
 }
 
+router.get('/chatting/students', getChattingStudent);
+function getChattingStudent(req, res, next) {
+    fs.readFile(__dirname + '/../views/chatting_student.html', (err, data) => {
+        res.end(data, 'utf-8');
+    });
+}
 
 module.exports = router;
