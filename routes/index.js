49bec4057ddf73cffc94192760b2c244df55617a
BaeJi77 <hahawjstk@gmail.com>
2019-10-31 20:35:41 +0900

    Return to original condition

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const fs = require('fs');
 
 
-// router.get('/', getIndex);
-//
-//
-// function getIndex (req, res, next) {
-//     res.render('index', { title: 'Express' });
-// }
+router.get('/', getIndex);
 
-router.get('/chatting/trainers', getChattingTrainer);
-function getChattingTrainer(req, res, next) {
-    fs.readFile(__dirname + '/../views/chatting_trainer.html', (err, data) => {
-        res.end(data, 'utf-8');
-    });
+
+function getIndex (req, res, next) {
+    res.render('index', { title: 'Express' });
 }
 
-router.get('/chatting/students', getChattingStudent);
-function getChattingStudent(req, res, next) {
-    fs.readFile(__dirname + '/../views/chatting_student.html', (err, data) => {
-        res.end(data, 'utf-8');
-    });
-}
 
 module.exports = router;
