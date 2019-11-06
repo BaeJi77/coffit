5deb00ac08b453d7633f8a9a6958785fe4f7e92e
BaeJi77 <hahawjstk@gmail.com>
2019-11-06 14:50:55 +0900

    Add cache logic in banner repository

--- repositories/bannerRepository.js
+++ repositories/bannerRepository.js
@@ -1,10 +1,23 @@
 const {Banner} = require('../models');
 
+const redis = require('../config/redis');
+
 module.exports = {
     findAllBanner: async function() {
-        return await Banner.findAll({
-            raw: true
-        });
+        return redis.redisClient.get('findAllBanner')
+            .then(async (res) => {
+                console.log(res);
+                if(!res) {
+                    let findBannerResult = await Banner.findAll({
+                        raw: true
+                    });
+                    console.log(findBannerResult);
+                    redis.redisClient.set('findAllBanner', JSON.stringify(findBannerResult), 'EX', redis.expireTimeOn1Hour);
+                    return findBannerResult;
+                } else {
+                    return JSON.parse(res);
+                }
+            });
     },
 
     createNewBanner: async function(newBannerInformation) {
